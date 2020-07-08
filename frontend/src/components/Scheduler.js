e5757cd84ac5d84e8fe0a34af175f113c65993da
Trifkovic Jelena <trifkovicjelena96@gmail.com>
2020-07-08 12:49:07 +0200

    Scheduler add Logout, add CRUD forms

--- frontend/src/components/Scheduler.js
+++ frontend/src/components/Scheduler.js
@@ -4,22 +4,30 @@ import { Calendar } from 'antd';
 import { Badge } from 'antd';
 import { Alert } from 'antd';
 import { Button } from 'antd';
+import { Space } from 'antd';
+import { Typography } from 'antd';
+import { Card } from 'antd';
+import { Form } from 'antd';
+import { Input } from 'antd';
 import moment from 'moment';
 
-const logOutButton = <Button style={{ margin: "auto" }}>Log out</Button>;
+const { Text } = Typography;
+
+const logOutButton = <Button style={{ marginRight: "2vw" }}>Log out</Button>;
 
 function getDayData(value) {
-    console.log(value);
+    //console.log(value);
     let event;
 
     const day = value.date();
     const month = value.month();
     const year = value.year();
 
-    console.log(day);
-    console.log(month);
-    console.log(year);
+    //console.log(day);
+    //console.log(month);
+    //console.log(year);
 
+    // ENDPOINT 2 - DISPLAY EVENT PER DAY
     switch (day) {
         case 8:
             event = { type: 'success', content: 'Event 1' };
@@ -44,10 +52,6 @@ function dateCellRender(value) {
 
 const { TabPane } = Tabs;
 
-function onPanelChange(value, mode) {
-    console.log(value.format('YYYY-MM-DD'), mode);
-}
-
 function callback(key) {
     console.log(key);
 }
@@ -57,32 +61,116 @@ class Scheduler extends React.Component {
     state = {
         value: moment(),
         selectedValue: moment(),
+        selected: false,
+        editDeleteEvent: false,
+        createEvent: false
     };
 
     onSelect = value => {
+        console.log(`onSelect -> ${value.format('YYYY-MM-DD')}`);
         this.setState({
             value,
             selectedValue: value,
+            selected: true
         });
+
+        // ENDPOINT 2 - CREATE/EDIT/DELETE
     };
 
     onPanelChange = value => {
-        this.setState({ value });
+        console.log(`onPanelChange -> ${value.format('YYYY-MM-DD')}`);
+        this.setState({ 
+            value
+        });
+    };
+
+    onFinish = values => {
+        console.log('Received values of form: ', values);
     };
 
     render() {
-        const { value, selectedValue } = this.state;
+        const { value, selectedValue, selected, editDeleteEvent, createEvent } = this.state;
+
+        let alert;
+        if (selected) {
+            alert = <Alert
+                message={`You selected date: ${selectedValue && selectedValue.format('YYYY-MM-DD')}`}
+                style={{
+                    width: "60%",
+                    margin: "auto"
+            }}/>;
+        } else {
+            alert = '';
+        }
+
+        let createForm;
+        if (createEvent) {
+            createForm = <Card style={{ width: "60%", margin: "auto", marginTop: "2vh" }}>
+                <Space direction="vertical">
+                <Text type="primary">Do you want to create the event?</Text>
+                <Form initialValues={{ remember: true }}
+                    onFinish={this.onFinish}
+                >
+                    <Form.Item
+                        name="eventName"
+                        rules={[{ required: true, message: 'Please input your event name!' }]}
+                    >
+                        <Input placeholder="Event name" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary">Create</Button>
+                    </Form.Item>
+                </Form>
+            </Space>
+            </Card>;
+        } else {
+            createForm = '';
+        }
+
+        let editForm;
+        if (editDeleteEvent) {
+            editForm = <Card style={{ width: "60%", margin: "auto", marginTop: "2vh" }}>
+                <Space direction="vertical">
+                <Text type="primary">Do you want to edit the event?</Text>
+                <Form initialValues={{ remember: true }}
+                    onFinish={this.onFinish}
+                >
+                    <Form.Item
+                        name="eventName"
+                        rules={[{ required: true, message: 'Please input your event name!' }]}
+                    >
+                        <Input placeholder="Event name" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button>Edit</Button>
+                    </Form.Item>
+                </Form>
+            </Space>
+            </Card>;
+        } else {
+            editForm = '';
+        }
+
+        let deleteForm;
+        if (editDeleteEvent) {
+            deleteForm = <Card style={{ width: "60%", margin: "auto", marginTop: "2vh" }}>
+                <Space direction="vertical">
+                <Text type="danger">Do you want to delete event?</Text>
+                <Button type="primary" danger>Delete</Button>
+            </Space>
+            </Card>;
+        } else {
+            deleteForm = '';
+        }
+
         return (
             <Tabs defaultActiveKey="1" onChange={callback} tabBarExtraContent={logOutButton} centered>
                 <TabPane tab="Calendar" key="1">
-                    <Alert
-                        message={`You selected date: ${selectedValue && selectedValue.format('YYYY-MM-DD')}`}
-                        style={{
-                            width: "60%",
-                            margin: "auto"
-                        }}
-                    />
-                    <Calendar onPanelChange={onPanelChange}
+                    { alert }
+                    { createForm }
+                    { editForm }
+                    { deleteForm }
+                    <Calendar onPanelChange={this.onPanelChange}
                         dateCellRender={dateCellRender}
                         value={value}
                         onSelect={this.onSelect}
