574e32fc80a55a730b1c4bd572f61022b8b535ac
Trifkovic Jelena <trifkovicjelena96@gmail.com>
2020-07-08 15:49:51 +0200

    backend: User and Event models added

---
+++ backend/scheduling/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class User(models.Model):
+    username = models.CharField(max_length=30, unique=True)
+    password = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f'{self.username}, {self.password}'
+
+class Event(models.Model):
+    name = models.CharField(max_length=30)
+    date = models.DateField()
+    user = models.ForeignKey('User', on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f'{self.name}, {self.date}, User({self.user})'
